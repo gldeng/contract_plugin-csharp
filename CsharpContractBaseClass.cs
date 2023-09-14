0cfb995826310b5b728e18665ee3ccf4a2c58178
Aeshan Wijetunge <aeshan.w@aelf.io>
2023-09-14 10:53:38 +0800

    initial setup of .NET project

---
+++ CsharpContractBaseClass.cs
@@ -0,0 +1,16 @@
+using Google.Protobuf;
+using Google.Protobuf.Compiler;
+using Google.Protobuf.Reflection;
+
+namespace contract_plugin_csharp{
+    class CSharpContractBaseClass{
+        public CSharpContractBaseClass()
+        {
+            //TODO setup steps
+        }
+        public string Generate(CodeGeneratorRequest request)
+        {
+            return "";
+        }
+    }
+}
