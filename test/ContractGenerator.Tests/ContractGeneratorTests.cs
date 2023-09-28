ffde15a51b25ad41547ef9433db3dd3213ed2bf9
gldeng <realgldeng@gmail.com>
2023-09-28 23:38:06 +0800

    test: add FileDescriptorSet generation and usage in unit tests

---
+++ test/ContractGenerator.Tests/ContractGeneratorTests.cs
@@ -0,0 +1,28 @@
+using Google.Protobuf.Reflection;
+using Xunit.Abstractions;
+
+namespace ContractGenerator.Tests;
+
+public class ContractGeneratorTests
+{
+    private readonly ITestOutputHelper _output;
+
+    public ContractGeneratorTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    private FileDescriptorSet GetFileDescriptorSet(string testcaseName)
+    {
+        var descriptor = File.ReadAllBytes($@"testcases/{testcaseName}/descriptor.bin");
+        return FileDescriptorSet.Parser.ParseFrom(descriptor);
+    }
+
+    [Fact]
+    void Test()
+    {
+        var fds = GetFileDescriptorSet("helloworld");
+        var filenames = string.Join("\n", fds.File.Select(f => f.Name));
+        _output.WriteLine($"Got files:\n {filenames}");
+    }
+}
