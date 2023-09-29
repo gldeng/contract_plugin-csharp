4b67f8e261ccc72f19eeae123966961491a8260e
gldeng <realgldeng@gmail.com>
2023-09-29 14:48:39 +0800

    test: add custom options retrieval

--- test/ContractGenerator.Tests/ProtoUtilsTests.cs
+++ test/ContractGenerator.Tests/ProtoUtilsTests.cs
@@ -8,10 +8,22 @@ namespace ContractGenerator.Tests;
 
 public class ProtoUtilsTests
 {
+    private static readonly ExtensionRegistry _extensionRegistry = new ExtensionRegistry();
+
+    static ProtoUtilsTests()
+    {
+        _extensionRegistry.Add(AElf.OptionsExtensions.Identity);
+        _extensionRegistry.Add(AElf.OptionsExtensions.Base);
+        _extensionRegistry.Add(AElf.OptionsExtensions.CsharpState);
+        _extensionRegistry.Add(AElf.OptionsExtensions.IsView);
+        _extensionRegistry.Add(AElf.OptionsExtensions.IsEvent);
+        _extensionRegistry.Add(AElf.OptionsExtensions.IsIndexed);
+    }
+
     private FileDescriptorSet GetFileDescriptorSet(string testcaseName)
     {
         var descriptor = File.ReadAllBytes($@"testcases/{testcaseName}/descriptor.bin");
-        return FileDescriptorSet.Parser.ParseFrom(descriptor);
+        return FileDescriptorSet.Parser.WithExtensionRegistry(_extensionRegistry).ParseFrom(descriptor);
     }
 
     [Theory]
@@ -60,7 +72,13 @@ public class ProtoUtilsTests
         // var fileDescriptors = FileDescriptor.BuildFromByteStrings(new []{
         //     aelfOptionsDescriptor,emptyDescRef,wrappersDescRef,descriptorRef,fds.File[0].ToByteString()
         // });
-        var fileDescriptors = FileDescriptor.BuildFromByteStrings(byteStrings);
+        // var fileDescriptors = FileDescriptor.BuildFromByteStrings(byteStrings);
+
+        var fileDescriptors = FileDescriptor.BuildFromByteStrings(byteStrings, _extensionRegistry);
+        var svc = fileDescriptors.Last().Services.Last();
+
+        var opt = svc.GetOptions();
+        var state = opt.GetExtension(AElf.OptionsExtensions.CsharpState);
         // Act: Call the GetClassName method
         var className = ProtoUtils.GetClassName(fileDescriptors[0]);
 
