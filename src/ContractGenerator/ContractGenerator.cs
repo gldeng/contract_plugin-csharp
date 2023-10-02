b38722f96935317cbdaa384edf0bf9f9d3cd2b01
Aeshan Wijetunge <aeshan.w@aelf.io>
2023-10-02 14:26:40 +0800

    feat: implement generateEvent function body with stubs

--- src/ContractGenerator/ContractGenerator.cs
+++ src/ContractGenerator/ContractGenerator.cs
@@ -1,4 +1,5 @@
 using Google.Protobuf.Compiler;
+using Google.Protobuf.Reflection;
 
 namespace ContractGenerator;
 
@@ -12,5 +13,88 @@ public class ContractGenerator
     public CodeGeneratorResponse Generate(Stream stdin)
     {
         throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Determines if the proto-message is of EventType based on Aelf.options
+    /// </summary>
+    private static bool IsEventMessageType(MessageDescriptor message)
+    {
+        return message.GetOptions().GetExtension(AElf.OptionsExtensions.IsEvent);
+    }
+
+    /// <summary>
+    /// Determines if the proto-message is of IndexedType based on Aelf.options
+    /// </summary>
+    private static bool IsIndexedField(FieldDescriptor field)
+    {
+        return field.GetOptions().GetExtension(AElf.OptionsExtensions.IsIndexed);
+    }
+
+    private static string GetAccessLevel(byte flags)
+    {
+        throw new NotImplementedException();
+    }
+    public static void GenerateEvent(ref IndentPrinter printer, MessageDescriptor message, byte flags)
+    {
+        if (!IsEventMessageType(message)) return;
+        printer.Print($"{GetAccessLevel(flags)} partial class {message.Name} : aelf::IEvent<{message.Name}>");
+        printer.Print("{");
+        {
+            printer.Indent();
+            // GetIndexed
+            printer.Print($"public global::System.Collections.Generic.IEnumerable<{message.Name}> GetIndexed()");
+            printer.Print("{");
+            {
+                printer.Indent();
+                printer.Print($"return new List<{message.Name}>");
+                printer.Print("{");
+                var fields = message.Fields.InFieldNumberOrder();
+                foreach (var field in fields)
+                {
+                    if (!IsIndexedField(field)) continue;
+                    printer.Print($"new {message.Name}");
+                    printer.Print("{");
+                    {
+                        printer.Indent();
+                        var propertyName = ProtoUtils.GetPropertyName(field);
+                        printer.Print($"{propertyName} = {propertyName}");
+                        printer.Outdent();
+                    }
+                    printer.Print("},");
+                }
+
+                printer.Print("};");
+                printer.Outdent();
+            }
+            printer.Print("}\n"); // end GetIndexed
+
+            // GetNonIndexed
+            printer.Print($"public {message.Name} GetNonIndexed()");
+            printer.Print("{");
+            {
+                printer.Indent();
+                printer.Print($"return new {message.Name}");
+                printer.Print("{");
+                {
+                    printer.Indent();
+                    var fields = message.Fields.InFieldNumberOrder();
+                    foreach (var field in fields)
+                    {
+                        if (IsIndexedField(field)) continue;
+                        var propertyName = ProtoUtils.GetPropertyName(field);
+                        printer.Print($"{propertyName} = {propertyName},");
+                    }
+
+                    printer.Outdent();
+                }
+                printer.Print("};");
+                printer.Outdent();
+            }
+            printer.Print("}");
+            printer.Outdent();
+        }
+
+        printer.Print("}\n");
     }
 }
