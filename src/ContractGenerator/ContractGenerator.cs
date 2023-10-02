6f3ac783c0236b1e1ca821c43527793b7cb91760
Aeshan Wijetunge <aeshan.w@aelf.io>
2023-10-02 14:46:42 +0800

    feat: implement GetAccessLevel method

--- src/ContractGenerator/ContractGenerator.cs
+++ src/ContractGenerator/ContractGenerator.cs
@@ -1,7 +1,20 @@
+using AElf;
 using Google.Protobuf.Compiler;
 using Google.Protobuf.Reflection;
 
 namespace ContractGenerator;
+
+public class FlagConstants
+{
+    public const byte GenerateContract = 0x01; // hex for 0000 0001
+    public const byte GenerateStub = 0x02; // hex for 0000 0010
+    public const byte GenerateReference = 0x04; // hex for 0000 0100
+    public const byte GenerateEvent = 0x08; // hex for 0000 1000
+    public const byte InternalAccess = 0x80; // hex for 1000 0000
+
+    public const byte GenerateContractWithEvent = GenerateContract | GenerateEvent;
+    public const byte GenerateStubWithEvent = GenerateStub | GenerateEvent;
+}
 
 //This is the main entry-point into this project and is exposed to external users
 public class ContractGenerator
@@ -16,29 +29,26 @@ public class ContractGenerator
     }
 
     /// <summary>
-    /// Determines if the proto-message is of EventType based on Aelf.options
+    ///     Determines if the proto-message is of EventType based on Aelf.options
     /// </summary>
     private static bool IsEventMessageType(MessageDescriptor message)
     {
-        return message.GetOptions().GetExtension(AElf.OptionsExtensions.IsEvent);
+        return message.GetOptions().GetExtension(OptionsExtensions.IsEvent);
     }
 
     /// <summary>
-    /// Determines if the proto-message is of IndexedType based on Aelf.options
+    ///     Determines if the proto-message is of IndexedType based on Aelf.options
     /// </summary>
     private static bool IsIndexedField(FieldDescriptor field)
     {
-        return field.GetOptions().GetExtension(AElf.OptionsExtensions.IsIndexed);
+        return field.GetOptions().GetExtension(OptionsExtensions.IsIndexed);
     }
 
-    private static string GetAccessLevel(byte flags)
-    {
-        throw new NotImplementedException();
-    }
     public static void GenerateEvent(ref IndentPrinter printer, MessageDescriptor message, byte flags)
     {
         if (!IsEventMessageType(message)) return;
-        printer.Print($"{GetAccessLevel(flags)} partial class {message.Name} : aelf::IEvent<{message.Name}>");
+        printer.Print(
+            $"{ProtoUtils.GetAccessLevel(flags)} partial class {message.Name} : aelf::IEvent<{message.Name}>");
         printer.Print("{");
         {
             printer.Indent();
